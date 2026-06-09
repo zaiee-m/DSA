663d25d9aeed636b169dd38e1721de3e822dff50
zayeemB <zaiemgulzar43@gmail.com>
2026-06-09 15:57:00 +0530

    Add: find-median-in-a-data-stream - submission-0

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-0.cpp
@@ -0,0 +1,47 @@
+class MedianFinder {
+public:
+    priority_queue<int> max_heap;
+    priority_queue<int, vector<int>, greater<>> min_heap;
+
+    MedianFinder() {
+
+    }
+    
+    void addNum(int num) {
+
+        if(max_heap.empty() || num < max_heap.top()){
+            if(max_heap.size() > min_heap.size()){
+                min_heap.push(max_heap.top()); max_heap.pop();
+            }
+            max_heap.push(num);
+        }
+        else if(min_heap.empty() || num > min_heap.top()){
+            if(min_heap.size() > max_heap.size()){
+                max_heap.push(min_heap.top()); min_heap.pop();
+            }
+            min_heap.push(num);
+        }
+        else{
+            if(min_heap.size() == max_heap.size()) max_heap.push(num);
+            else if(min_heap.size() < max_heap.size()) min_heap.push(num);
+            else max_heap.push(num);
+        }
+    }
+    
+    double findMedian() {
+        if(max_heap.size() == min_heap.size()){
+            return (double)(max_heap.top()+min_heap.top())/2;
+        }
+        else{
+            if(min_heap.size() < max_heap.size()) return max_heap.top();
+            else return min_heap.top();
+        }
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
